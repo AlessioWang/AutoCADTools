0a3ad08bf5d140a7e94cca0edb51ed5bd7739520
AlessioWang <1013797789@qq.com>
2021-07-26 13:40:16 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace AutoCADLearning
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        [CommandMethod("helloWorld", CommandFlags.Session)]
+        public void HelloWorld()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("\n Hello World");
+        }
+
+
+        [CommandMethod("newMethod", CommandFlags.Session)]
+        public void Method1()
+        {
+
+        }
+    }
+}
